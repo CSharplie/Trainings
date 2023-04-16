de8d1a7acea09623f0743342943afa995ec9a660
Charlie Collier <charlie.collier@outlook.fr>
2023-04-16 21:47:12 +0200

    Clean configuration

--- modules/server/server.tf
+++ modules/server/server.tf
@@ -27,106 +27,10 @@ resource "azurerm_sql_firewall_rule" "db_fw_all" {
   end_ip_address      = "255.255.255.255"
 }
 
-module "database_01" {
+module "database_00" {
   source         = "../../modules/database"
   resource_group = var.resource_group
   environment    = var.environment
-  suffix         = "MBE"
-  server         = azurerm_sql_server.server
-}
-
-module "database_02" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "SCI"
-  server         = azurerm_sql_server.server
-}
-
-module "database_03" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "ADH"
-  server         = azurerm_sql_server.server
-}
-
-module "database_04" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "AHA"
-  server         = azurerm_sql_server.server
-}
-
-module "database_05" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "NGR"
-  server         = azurerm_sql_server.server
-}
-
-module "database_06" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "NGE"
-  server         = azurerm_sql_server.server
-}
-
-module "database_07" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "YKA"
-  server         = azurerm_sql_server.server
-}
-
-module "database_08" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "AKH"
-  server         = azurerm_sql_server.server
-}
-
-module "database_09" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "BLY"
-  server         = azurerm_sql_server.server
-}
-
-module "database_10" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "AMO"
-  server         = azurerm_sql_server.server
-}
-
-module "database_11" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "YPO"
-  server         = azurerm_sql_server.server
-}
-
-module "database_12" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "TTI"
-  server         = azurerm_sql_server.server
-}
-
-module "database_13" {
-  source         = "../../modules/database"
-  resource_group = var.resource_group
-  environment    = var.environment
-  suffix         = "ARO"
+  suffix         = "CCO"
   server         = azurerm_sql_server.server
 }
