f31b0a711f6b77aac9a6c1755fb91a4556464440
CSharplie <charlie.collier@outlook.fr>
2023-05-22 22:06:48 +0200

    Rename AAS server

--- CICD with AAS/Azure/setup.tf
+++ CICD with AAS/Azure/setup.tf
@@ -79,7 +79,7 @@ resource "azurerm_sql_firewall_rule" "db_fw_all" {
 }
 
 resource "azurerm_analysis_services_server" "aas" {
-  name                = "training-cicd-analytics-aas-${var.environment}"
+  name                = "trainingcicdanalyticsaas${var.environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "D1"
